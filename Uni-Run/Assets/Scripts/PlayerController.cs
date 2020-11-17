cba489ded02a7782621ee63c6c1ed7991184b48d
Hansle Lee <hssarah13@gmail.com>
2020-11-18 01:35:57 +0900

    완성!
    빌드전

--- Uni-Run/Assets/Scripts/PlayerController.cs
+++ Uni-Run/Assets/Scripts/PlayerController.cs
@@ -54,8 +54,9 @@ public class PlayerController : MonoBehaviour {
    }
 
    private void OnTriggerEnter2D(Collider2D other) {
-       // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
-       if(other.tag == "Dead" && !isDead)
+        // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+        //if(other.tag == "Dead" && !isDead)
+        if (other.tag == "Dead" && !isDead)
         {
             Die();
         }
